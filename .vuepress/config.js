7bd1c611247072b6e4ecafad154d66a1cc8afb46
GwangSeub Ma <hellomrma@gmail.com>
2021-11-07 17:36:41 +0900

    Update content

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,12 +1,12 @@
 // const sidebar = require('./sidebar');
 
 module.exports = {
-  title: 'React',
-  description: '인프런 React 강의 수강하면서 간단하게 정리한 내용',
+  title: 'Typescript',
+  description: '인프런 Typescript 강의 수강하면서 간단하게 정리한 내용',
   themeConfig: {
     nav: [
       { text: 'github@hellomrma', link: 'https://github.com/hellomrma' },
-      { text: 'React 공식', link: 'https://ko.reactjs.org/' },
+      { text: 'Typescript 공식', link: 'https://www.typescriptlang.org/' },
     ],
     sidebar: 'auto'
   }
